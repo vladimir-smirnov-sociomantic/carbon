3363312f226fbf60fa5252a428d3b40ebaacb634
crazy format <crazy.format@gmail.com>
2014-01-09 20:01:32 +0400

    new rollup plugin

--- plugins/maintenance/rollup.py
+++ plugins/maintenance/rollup.py
@@ -1,15 +1,22 @@
+import logging
 import time
-from ceres import CeresSlice, SliceDeleted
-
+import json
+from ceres import CeresSlice, SliceDeleted, SliceGapTooLarge, NoData
 #######################################################
 # Put your custom aggregation logic in this function! #
 #######################################################
-def aggregate(node, datapoints):
-  "Put your custom aggregation logic here."
-  values = [value for (timestamp,value) in datapoints if value is not None]
-  metadata = node.readMetadata()
-  method = metadata.get('aggregationMethod', 'avg')
 
+date = time.strftime("%Y-%m-%d %H:%M:%S")
+LOG = "/var/log/ceres_rollup/ceres_rollup.err"
+logger = logging.getLogger("rollup")
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(LOG)
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s  %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+def aggregate(method, values):
   if method in ('avg', 'average'):
     return float(sum(values)) / len(values) # values is guaranteed to be nonempty
 
@@ -26,12 +33,14 @@ def aggregate(node, datapoints):
     values.sort()
     return values[ len(values) / 2 ]
 
-
 def node_found(node):
   archives = []
   t = int( time.time() )
-  metadata = node.readMetadata()
-
+  try:
+    metadata = node.readMetadata()
+  except:
+    logger.error("%s failed to read metadata" % str(node))
+    return
   for (precision, retention) in metadata['retentions']:
     archiveEnd =  t - (t % precision)
     archiveStart = archiveEnd - (precision * retention)
@@ -40,138 +49,266 @@ def node_found(node):
       'precision' : precision,
       'retention' : retention,
       'startTime' : archiveStart,
-      'endTime' : archiveEnd,
-      'slices' : [s for s in node.slices if s.timeStep == precision]
+      'endTime'   : archiveEnd,
+      'slices'    : [s for s in node.slices if s.timeStep == precision]
     })
 
-  for i, archive in enumerate(archives):
-    if i == len(archives) - 1:
-      do_rollup(node, archive, None)
-    else:
-      do_rollup(node, archive, archives[i+1])
+  t = time.time()
+  do_rollup(node, archives, metadata.get('xFilesFactor'), metadata.get('aggregationMethod', 'avg'))
+  logger.info("%s rollup time: %.3f seconds" % (str(node), (time.time() - t)))
 
-
-
-def do_rollup(node, fineArchive, coarseArchive):
-  start_t = time.time()
-  overflowSlices = [s for s in fineArchive['slices'] if s.startTime < fineArchive['startTime']]
-  if not overflowSlices:
+def do_rollup(node, archives, xff, method):
+  # empty node?
+  if not archives:
     return
-  if coarseArchive is None: # delete the old datapoints
-    for slice in overflowSlices:
+
+  rollupStat = {}
+  for archive in archives:
+    rollupStat[archive['precision']] = {
+      'aggregate': 0,
+      'drop': 0,
+      'memory': 0,
+      'write': 0,
+      'slice_create': 0,
+      'slice_delete': 0,
+      'slice_delete_points': 0,
+      'slice_read': 0,
+      'slice_read_points': 0,
+      'slice_write': 0,
+      'slice_write_points': 0,
+      'slice_update': 0,
+      'slice_update_points': 0,
+    }
+
+  # list of (slice,deletePrioTo) -- will be dropped after aggregation
+  overflowSlices = []
+
+  # dict of in-memory aggregated points (one or more retentions skipped)
+  coarsePoints = {}
+
+  # start time of node ( = start time of lowest precision archive)
+  windowStart = archives[-1]['startTime']
+
+  # dropping data from lowest precision archive
+  fineStep = archives[-1]['precision']
+  for slice in archives[-1]['slices']:
+    if slice.startTime < windowStart:
+      overflowSlices.append((slice, windowStart))
+
+  for i in xrange(len(archives) - 1):
+    statTime = time.time()
+
+    # source archive for aggregation
+    fineArchive = archives[i]
+    fineStep    = fineArchive['precision']
+    fineStat    = rollupStat[fineStep]
+
+    # lower precision archive
+    coarseArchive = archives[i + 1]
+    coarseStep    = coarseArchive['precision']
+    coarseStart   = coarseArchive['startTime']
+    coarseStat    = rollupStat[coarseStep]
+
+    # end time for lower presicion archive ( = start time of source archive)
+    windowEnd = coarseArchive['endTime']
+
+    # reading points from source archive
+    finePoints = []
+    for slice in fineArchive['slices']:
+      # dropping data prior to start time of this archive
+      if windowStart > slice.endTime:
+        overflowSlices.append((slice, slice.endTime))
+        continue
+      # slice starts after lower precision archive ends -- no aggregation needed
+      if windowEnd <= slice.startTime:
+        continue
       try:
-        slice.deleteBefore(fineArchive['startTime'])
-      except SliceDeleted:
+        slicePoints = slice.read(max(windowStart, slice.startTime), windowEnd)
+        finePoints += [p for p in slicePoints if p[1] is not None]
+
+        fineStat['slice_read']        += 1
+        fineStat['slice_read_points'] += len(slicePoints)
+      # no data in slice, just removing slice
+      except NoData:
         pass
 
-  else:
-    # some profiling
-    time_stat = {}
+      # dropping data, which aggregating right now
+      overflowSlices.append((slice, windowEnd))
 
-    start_t_tmp = time.time()
+    finePoints = dict(finePoints)
+    # adding in-memory aggregated data
+    finePoints.update(coarsePoints)
+    # sort by timestamp in ascending order
+    finePoints = sorted(finePoints.items())
+
+    coarsePoints = {}
+    # no points to aggregate :(
+    if not finePoints:
+      continue
+
+    # start time of aggregation (skipping already aggregated points)
+    startTime  = finePoints[0][0]
+    startTime -= startTime % coarseStep
+
+    # ... and last
+    endTime  = finePoints[-1][0]
+    endTime -= endTime % coarseStep
+    endTime += coarseStep
+
+    # since we are trying to write points in bulk and already existing slices
+    # we need a list of slice start/end times
+    # sliceEvents: list of (time, isEnd, slice-number)
+    sliceEvents   = []
     
-    overflowDatapoints = []
-    for slice in overflowSlices:
-      datapoints = slice.read(slice.startTime, fineArchive['startTime'])
-      overflowDatapoints.extend( list(datapoints) )
-    
-    time_stat['extended'] = time.time() - start_t_tmp
-    start_t_tmp = time.time()
+    # writeSlices: list of slices, where writePoints already exists
+    writeSlices   = []
+    # lastSeenSlice: slice with maximum endTime
+    # data will be written there with gap if no writeSlices found 
+    lastSeenSlice = None
+    for j in xrange(len(coarseArchive['slices'])):
+      slice = coarseArchive['slices'][j]
+      # slice starts after end of aggregation
+      if slice.startTime >= endTime:
+        continue
 
-    overflowDatapoints.sort()
-    fineStep = fineArchive['precision']
-    coarseStep = coarseArchive['precision']
-    deletePriorTo = coarseArchive['startTime'] + (coarseStep * coarseArchive['retention'])
-    
-    metadata = node.readMetadata()
-    xff = metadata.get('xFilesFactor')
+      # slice ended before start of aggregation -- it can be lastSeenSlice
+      if slice.endTime <= startTime:
+        if lastSeenSlice is None or lastSeenSlice.endTime < slice.endTime:
+          lastSeenSlice = slice
+        continue
 
-    time_stat['sorted'] = time.time() - start_t_tmp
-    start_t_tmp = time.time()
+      # starting point is not covered by slice -- adding start slice event
+      if slice.startTime > startTime:
+        sliceEvents.append((slice.startTime, False, j))
+      # starting point covered by slice
+      else:
+        writeSlices.append(j)
+      # adding end slice event
+      sliceEvents.append((slice.endTime, True, j))
+    # sort slice events by time
+    sliceEvents.sort()
 
-    # We define a window corresponding to exactly one coarse datapoint
-    # Then we use it to select datapoints for aggregation
+    sliceEventsIterator = iter(sliceEvents)
+    finePointsIterator = iter(finePoints)
 
-    # oldest timestamp in overflowDatapoints array is start point for moving through retention
+    # list of points with no gap between and no slice start/end events
+    # all these points will be written to one list of slices
+    writePoints = []
     try:
-        oldest_ts = overflowDatapoints[0][0]
-    except:
-        oldest_ts =0
-    # calculate the start point for retentions
-    start_p = int((oldest_ts - coarseArchive['startTime'])/coarseStep)
-    
-    # profiling variables
-    start_count = 0
-    fine_dp = 0
+      sliceEvent = next(sliceEventsIterator)
+    except StopIteration:
+      sliceEvent = None
 
-    for i in range(start_p, coarseArchive['retention']):
-    #for i in range(coarseArchive['retention']):
-      start_count += 1
-      start_t_in = time.time()
-      windowStart = coarseArchive['startTime'] + (i * coarseStep)
-      windowEnd = windowStart + coarseStep
-      fineDatapoints = [d for d in overflowDatapoints if d[0] >= windowStart and d[0] < windowEnd]
-      time_stat.setdefault(1, []).append(time.time() - start_t_in)
+    finePoint = next(finePointsIterator)
+    for ts in xrange(startTime, endTime, coarseStep):
+      tsEndTime = ts + coarseStep
 
-      if fineDatapoints:
-        time_stat.setdefault(6, []).append(time.time() - start_t_in)
-        fine_dp += 1
-        start_t_in = time.time()
-        knownValues = [value for (timestamp,value) in fineDatapoints if value is not None]
-        if not knownValues:
-          continue
-        knownPercent = float(len(knownValues)) / len(fineDatapoints)
-        if knownPercent < xff:  # we don't have enough data to aggregate!
-          continue
-        time_stat.setdefault(2, []).append(time.time() - start_t_in)
-        start_t_in = time.time()
-        
-        coarseValue = aggregate(node, fineDatapoints)
-        coarseDatapoint = (windowStart, coarseValue)
-        fineValues = [d[1] for d in fineDatapoints]
+      # no data for current timestamp -- next existing point is newer
+      if tsEndTime <= finePoint[0]:
+        # writing previously found points if needed
+        lastSeenSlice = write_points(node, coarseArchive, writePoints, writeSlices, lastSeenSlice, coarseStat)
+        writePoints = []
+        continue
 
-        time_stat.setdefault(3, []).append(time.time() - start_t_in)
-        start_t_in = time.time()
-        written = False
-        for slice in coarseArchive['slices']:
-          if slice.startTime <= windowStart and slice.endTime >= windowStart:
-            slice.write([coarseDatapoint])
-            written = True
-            break
-
-        time_stat.setdefault(4, []).append(time.time() - start_t_in)
-        start_t_in = time.time()
-          # We could pre-pend to an adjacent slice starting after windowStart
-          # but that would be much more expensive in terms of I/O operations.
-          # In the common case, append-only is best.
-
-        if not written:
-          newSlice = CeresSlice.create(node, windowStart, coarseStep)
-          newSlice.write([coarseDatapoint])
-          coarseArchive['slices'].append(newSlice)
-          deletePriorTo = min(deletePriorTo, windowStart)
-        time_stat.setdefault(5, []).append(time.time() - start_t_in)
-
-    time_stat['aggregated'] = time.time() - start_t_tmp
-    start_t_tmp = time.time()
-    # Delete the overflow from the fine archive
-    for slice in overflowSlices:
+      values = []
       try:
-        slice.deleteBefore( deletePriorTo ) # start of most recent coarse datapoint
-      except SliceDeleted:
+        # finding all values for current coarse point
+        while finePoint[0] < tsEndTime:
+          values.append(finePoint[1])
+          finePoint = next(finePointsIterator)
+      except StopIteration:
         pass
-    time_stat['overflow_delete'] = time.time() - start_t_tmp
-    time_stat['all'] = time.time() - start_t
 
-    out = ""
-    keys = [1, 2, 3, 4, 5, 6, 'overflow_delete', 'extended', 'sorted', 'aggregated', 'all']
-    for k in keys:
-        if not time_stat.has_key(k): continue
-        v = time_stat[k]
-        if type(v) == list:
-            out = "%s, %s: %.3f" % (out, k, sum(v))
-        else:
-            out = "%s, %s: %.3f" % (out, k, v)
-    print "node: %s, profiling - cycles: %d, fine_dp: %d, %s" % (node.fsPath, start_count, fine_dp, out)
+      fineStat['aggregate'] += 1
 
+      # checking xff
+      if float(len(values)) * fineStep / coarseStep < xff:
+        # writing previously found points if needed
+        lastSeenSlice = write_points(node, coarseArchive, writePoints, writeSlices, lastSeenSlice, coarseStat)
+        writePoints = []
 
+        fineStat['drop'] += 1
+        continue
+
+      newValue = aggregate(method, values)
+      # in-memory aggregated point (writePoints is empty since timestamps processed in ascending order)
+      if ts < coarseStart:
+        coarsePoints[ts] = newValue
+
+        fineStat['memory'] += 1
+        continue
+
+      # slice event found before current timestamp
+      if sliceEvent and sliceEvent[0] <= ts:
+        # since writeSlices changed -- writting all points
+        lastSeenSlice = write_points(node, coarseArchive, writePoints, writeSlices, lastSeenSlice, coarseStat)
+        writePoints = [(ts, newValue)]
+        # updating writeSlices add lastSeenSlice
+        try:
+          while sliceEvent[0] <= ts:
+            if sliceEvent[1]:
+              writeSlices.remove(sliceEvent[2])
+              lastSeenSlice = coarseArchive['slices'][sliceEvent[2]]
+            else:
+              writeSlices.append(sliceEvent[2])
+            sliceEvent = next(sliceEventsIterator)
+        except StopIteration:
+          sliceEvent = None
+      # no gaps, no events, just adding to list
+      else:
+        writePoints.append((ts, newValue))
+
+      fineStat['write'] += 1
+
+    # writing last portion of points
+    write_points(node, coarseArchive, writePoints, writeSlices, lastSeenSlice, coarseStat)
+
+    fineStat['time'] = time.time() - statTime
+
+  # after all -- drop aggregated data from source archives
+  for slice, deletePriorTo in overflowSlices:
+    try:
+      rollupStat[slice.timeStep]['slice_delete']        += 1
+      rollupStat[slice.timeStep]['slice_delete_points'] += (min(slice.endTime, deletePriorTo) - slice.startTime) / slice.timeStep
+      slice.deleteBefore(deletePriorTo)
+    except SliceDeleted:
+      pass
+
+  logger.info("%s rollup stat: %s" % (str(node), json.dumps(rollupStat)))
+
+def write_points(node, archive, points, slices, lastSlice, stat):
+  if not points:
+    return lastSlice
+
+  written = False
+  # trying to update all existing slices
+  for i in slices:
+    try:
+      archive['slices'][i].write(points)
+      written = True
+
+      stat['slice_update']        += 1
+      stat['slice_update_points'] += len(points)
+    except SliceDeleted:
+      pass
+  # if not -- writing to lastSeenSlice with gap
+  if not written and lastSlice:
+    try:
+      lastSlice.write(points)
+      written = True
+
+      stat['slice_write']        += 1
+      stat['slice_write_points'] += len(points)
+    except (SliceDeleted,SliceGapTooLarge):
+      pass
+  # gap in last slice too large -- creating new slice
+  if not written:
+    newSlice = CeresSlice.create(node, points[0][0], archive['precision'])
+    newSlice.write(points)
+    archive['slices'].append(newSlice)
+    lastSlice = newSlice
+
+    stat['slice_create']       += 1
+    stat['slice_write']        += 1
+    stat['slice_write_points'] += len(points)
+  return lastSlice
