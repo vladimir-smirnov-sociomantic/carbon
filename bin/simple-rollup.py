42c566674e70b0c25db062d465c2519b8ecb3de8
dkulikovsky <dkulikovsky@yandex-team.ru>
2014-04-07 13:54:15 +0400

    wrapped output to log_msg

--- bin/simple-rollup.py
+++ bin/simple-rollup.py
@@ -5,6 +5,9 @@ from multiprocessing import Pool
 from os.path import join, exists
 from optparse import OptionParser
 
+def log_msg(lvl, msg):
+    print "%s [%s] %s" % (time.ctime, lvl.lower(), msg)
+
 lock_file = '/var/tmp/simple-rollup.lock'
 # Make carbon imports available for some functionality
 root_dir = "/var/lib/graphite"
@@ -14,7 +17,7 @@ sys.path.append(join(root_dir,"lib"))
 try:
   import carbon
 except ImportError:
-  print ("Failed to import carbon, specify your installation location "
+  log_msg("fatal","Failed to import carbon, specify your installation location "
          "with the GRAPHITE_ROOT environment variable.")
   sys.exit(1)
 
@@ -44,22 +47,22 @@ if __name__ == '__main__':
         lock_timeout = lock_timeout - 1 
         time.sleep(1)
       else:
-        print "can't get lock, reason: %s" % e[1]
+        log_msg("fatal", "can't get lock, reason: %s" % e[1])
         sys.exit(1)
     except:
-      print "failed to get lock for some unknown reason"
+      log_msg("fatal", "failed to get lock for some unknown reason")
       sys.exit(1)
 
   if not got_lock:
-    print "Failed to get lock for 60s"
+    log_msg("fatal", "Failed to get lock for 60s")
     sys.exit(1)
 
   tree = getTree(root)
   if tree is None:
-    print "%s is not inside a CeresTree" % root
+    log_msg("fatal", "%s is not inside a CeresTree" % root)
     sys.exit(1)
 
-  print "Starting rollup"
+  log_msg("info", "Starting rollup")
   nodes_found = 0
   exec_time = time.time()
   if options.metric:
@@ -81,8 +84,8 @@ if __name__ == '__main__':
   
     proc_pool.close()
     proc_pool.join()
-    print "found %s nodes" % nodes_found
-  print "All work is done"
+    log_msg("info", "found %s nodes" % nodes_found)
+  log_msg("info", "All work is done")
   
 
 
